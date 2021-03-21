c35727592b45e40764168c898b504e47c5f30782
Jeremy Lim <jeremylimjw@gmail.com>
2021-03-22 07:13:24 +0800

    Completed cancel appointment use case (#3)
    * Completed cancel appointment use case

--- src/app/view-appointment/view-appointment.component.ts
+++ src/app/view-appointment/view-appointment.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { MessageService, TypeEnum } from '../services/message.service';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-view-appointment',
   templateUrl: './view-appointment.component.html',
   styleUrls: ['./view-appointment.component.css']
 })
-export class ViewAppointmentComponent implements OnInit {
+export class ViewAppointmentComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ViewAppointmentComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: {appointment: any},
+    private messageService: MessageService,
+    private apiService: ApiService) { }
 
-  appointment: any;
-
-  constructor(private apiService: ApiService, private route: ActivatedRoute) { }
-
-  ngOnInit(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.apiService.getAppointmentById(id)
-      .subscribe(result => this.appointment = result);
+  getBasicRate() {
+    return this.data.appointment.total_price - this.data.appointment.prescriptions.reduce((a,b) => a + (b.medication.price_per_quantity * b.quantity), 0);
   }
 
+  cancelAppointment() {
+    this.apiService.cancelAppointment(this.data.appointment.id).subscribe(res => {
+      this.messageService.addTemporaryMessage(TypeEnum.Success, "Appointment has been cancelled.", "ID " + res);
+      this.dialogRef.close();
+    });
+  }
 }
