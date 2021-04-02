098f8eb36a11b6041c6c6016f33cb2ad365870d1
Jeremy <jeremylimjw@gmail.com>
2021-04-03 02:05:11 +0800

    Completed change password

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { ManageAccountComponent } from '../manage-account/manage-account.component';
+import { ApiService } from '../services/api.service';
+import { AuthService } from '../services/auth.service';
+import { MessageService, TypeEnum } from '../services/message.service';
 
 @Component({
   selector: 'app-change-password',
@@ -16,9 +19,32 @@ export class ChangePasswordComponent implements OnInit {
     cPassword: new FormControl('', Validators.required),
   });
 
-  constructor(public dialogRef: MatDialogRef<ManageAccountComponent>, private formBuilder: FormBuilder) { }
+  constructor(
+    public dialogRef: MatDialogRef<ManageAccountComponent>, 
+    private formBuilder: FormBuilder, 
+    private apiService: ApiService, 
+    private authService: AuthService,
+    private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.changeForm.get("cPassword").valueChanges.subscribe(x => {
+      if (x.length == 0 || x != this.changeForm.get("password").value) {
+        this.changeForm.get("cPassword").setErrors({'incorrect': true});
+      } else {
+        this.changeForm.get("cPassword").setErrors(null);
+      }
+    });
+  }
+
+  onSubmit(): void {
+    this.apiService.changePassword({ 
+      id: this.authService.getUser().id, 
+      old_password: this.changeForm.get('oPassword').value, 
+      password: this.changeForm.get('password').value
+    }).subscribe(res => {
+      this.messageService.addTemporaryMessage(TypeEnum.Success, "Password successfully changed!", "Password changed successfully");
+      this.dialogRef.close();
+    })
   }
 
 }
