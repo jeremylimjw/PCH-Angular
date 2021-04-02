b78cd4941b8a45d3ffa614d18ad29b4da2099efd
NgJunKai <e0407444@u.nus.edu>
2021-04-02 23:58:45 +0800

    added in edit account

---
+++ src/app/edit-account/edit-account.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-account',
+  templateUrl: './edit-account.component.html',
+  styleUrls: ['./edit-account.component.css']
+})
+export class EditAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    
+
+
+  }
+
+}
