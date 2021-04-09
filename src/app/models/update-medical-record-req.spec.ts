43587ffdb61a3c3a9dd116972bddaef80ff5370f
NgJunKai <e0407444@u.nus.edu>
2021-04-10 04:54:10 +0800

    Cleaning up data

--- src/app/models/update-medical-record-req.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UpdateMedicalRecordReq } from './update-medical-record-req';
-
-describe('UpdateMedicalRecordReq', () => {
-  it('should create an instance', () => {
-    expect(new UpdateMedicalRecordReq()).toBeTruthy();
-  });
-});
